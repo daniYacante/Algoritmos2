732e528865404962dc1243030c1ce5710d837b31
daniYacante <git@github.com>
2023-03-28 21:27:34 -0300

    agregado funcion showTrieContent

--- practicas/tp-trie/code/trie.py
+++ practicas/tp-trie/code/trie.py
@@ -8,17 +8,40 @@ class TrieNode:
 	key=None
 	isEndOfWord=False
 
+def __showTrie(nl:List[TrieNode],cadenas:list,palabra:str):
+	for node in nl:
+		if node.isEndOfWord:
+			cadenas.append(palabra+node.key)
+		if node.children!=None:
+			__showTrie(node.children,cadenas,palabra+node.key)
+	return
+def showTrieContent(T:Trie):
+	if T.root!=None:
+		content=[]
+		__showTrie(T.root,content,"")
+		return content
+	else:
+		return None
 
 def __insert(lista:list,element:str):
-	newNode=TrieNode()
-	newNode.key=element[0]
-	if len(element)!=1:
-		newNode.children=[]
-		__insert(newNode.children,element[1:])
+	nodo=__searchInList(lista,element)
+	if nodo!=None:
+		if len(element)>1:
+			if nodo.children==None:
+				nodo.children=[]
+			__insert(nodo.children,element[1:])
+		else:
+			nodo.isEndOfWord=True
 	else:
-		newNode.isEndOfWord=True
-	lista.append(newNode)
-	return
+		newNode=TrieNode()
+		newNode.key=element[0]
+		if len(element)!=1:
+			newNode.children=[]
+			__insert(newNode.children,element[1:])
+		else:
+			newNode.isEndOfWord=True
+		lista.append(newNode)
+		return
 
 def __searchInList(lista:List[TrieNode],element:str):
 	for nodo in lista:
@@ -39,4 +62,9 @@ def delete(T:Trie,element:str):
 	return
 
 A=Trie()
+insert(A,"holo")
 insert(A,"hola")
+insert(A,"holanda")
+insert(A,"hipopotamo")
+insert(A,"pez")
+print(showTrieContent(A))
