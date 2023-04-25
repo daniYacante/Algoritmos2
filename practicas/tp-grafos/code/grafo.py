9c7fa7b3aa6d02a25144580bc707d1d76b75e826
daniYacante <danizyacante@gmail.com>
2023-04-25 12:48:07 -0300

    agregado existPath

--- practicas/tp-grafos/code/grafo.py
+++ practicas/tp-grafos/code/grafo.py
@@ -27,31 +27,28 @@ class grafo():
 				cad+=", ".join(str(vec) for vec in n.vecinos)
 			cad+="\n"
 		return cad
-
+	def _getPos(self,key):
+		enc=False
+		for i in range(self.m):
+			pos=self.fhash(key,i)
+			if self.nodos[pos].value==key:
+				enc=True
+				break
+		if not enc:
+			# print(f"Vértice {x} no encontrado")
+			return None
+		else:
+			return pos
 	def addArista(self,arista:tuple):
 		x,y=arista
-		xEn=False
-		yEn=False
-		for i in range(self.m):
-			posx=self.fhash(x,i)
-			if self.nodos[posx].value==x:
-				xEn=True
-				break
-		for j in range(self.m):
-			posy=self.fhash(y,j)
-			if self.nodos[posy].value==y:
-				yEn=True
-				break
-		if not xEn:
-			print(f"Vértice {x} no encontrado")
-		else:
+		posx=self._getPos(x)
+		posy=self._getPos(y)
+		if posx!=None:
 			if self.nodos[posx].vecinos != None:
 				self.nodos[posx].vecinos.append(y)
 			else:
 				self.nodos[posx].vecinos=[y]
-		if not yEn:
-			print(f"Vértice {y} no encontrado")
-		else:
+		if posy!=None:
 			if self.nodos[posy].vecinos != None:
 				self.nodos[posy].vecinos.append(x)
 			else:
@@ -63,13 +60,30 @@ class grafNodo():
 	def __init__(self,value) -> None:
 		self.value=value
 		self.vecinos=None
+		self.visto=None
 
 def createGraph(vertices, aristas):
 	graph=grafo(vertices)
 	for ar in aristas:
 		graph.addArista(ar)
-	print(graph)
+	return graph
 
+def existPath(grafo:grafo,v1, v2):
+	posV1=grafo._getPos(v1)
+	if posV1!=None and not grafo.nodos[posV1].visto:
+		vec=grafo.nodos[posV1].vecinos
+		grafo.nodos[posV1].visto=True
+		if vec!=None:
+			for v in vec:
+				if v==v2:
+					return True
+			for v in vec:
+				
+				resp=existPath(grafo,v,v2)
+				if resp:
+					return resp
+		return False
 
-
-createGraph([1,2,5,3,8,7],[(1,2),(5,3),(5,8)])
+grafo=createGraph([1,2,3,4,5,6,7],[(1,2),(1,3),(2,3),(2,4),(5,4),(6,4)])
+print(existPath(grafo,5,7))
+print(grafo)
