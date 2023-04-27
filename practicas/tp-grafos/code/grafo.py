aa0a687bc1502bb2fe0559ce19eeb64f98bedb27
daniYacante <danizyacante@gmail.com>
2023-04-27 13:45:07 -0300

    agregado convertTree, cambios menores

--- practicas/tp-grafos/code/grafo.py
+++ practicas/tp-grafos/code/grafo.py
@@ -10,12 +10,15 @@ class grafo():
 		self.nodos=[None for n in nVertices]
 		self.m=len(nVertices)
 		self.vistos=[]
+		self.noVistos=[]
+		self.aristas=0
 		for v in nVertices:
 			IN=False
 			for i in range(self.m):
 				pos=self.fhash(v,i)
 				if self.nodos[pos]==None:
 					self.nodos[pos]=grafNodo(v)
+					self.noVistos.append(v)
 					IN=True
 					break
 			if not IN:
@@ -41,20 +44,27 @@ class grafo():
 			return None
 		else:
 			return pos
+	def getNumAristas(self):
+		return self.aristas
+	def getNumNodos(self):
+		return self.m
 	def addArista(self,arista:tuple):
 		x,y=arista
 		posx=self._getPos(x)
 		posy=self._getPos(y)
-		if posx!=None:
+		if posx!=None and posy!=None:
+			self.aristas+=1
 			if self.nodos[posx].vecinos != None:
 				self.nodos[posx].vecinos.append(y)
 			else:
 				self.nodos[posx].vecinos=[y]
-		if posy!=None:
 			if self.nodos[posy].vecinos != None:
 				self.nodos[posy].vecinos.append(x)
 			else:
 				self.nodos[posy].vecinos=[x]
+			return True
+		else:
+			return False
 		
 
 
@@ -62,80 +72,93 @@ class grafNodo():
 	def __init__(self,value) -> None:
 		self.value=value
 		self.vecinos=None
-		self.visto=None
 
-def createGraph(vertices, aristas):
+def createGraph(vertices:list, aristas:list):
 	graph=grafo(vertices)
 	for ar in aristas:
-		graph.addArista(ar)
+		if not graph.addArista(ar):
+			print(f"Arista {ar} no valida")
 	return graph
 
-def existPath(grafo:grafo,v1, v2):
-	posV1=grafo._getPos(v1)
-	if posV1!=None and not (grafo.nodos[posV1].value in grafo.vistos):
-		vec=grafo.nodos[posV1].vecinos
-		grafo.vistos.append(grafo.nodos[posV1].value)
+def existPath(graf:grafo,v1, v2):
+	posV1=graf._getPos(v1)
+	if posV1!=None and not (graf.nodos[posV1].value in graf.vistos):
+		vec=graf.nodos[posV1].vecinos
+		graf.vistos.append(graf.nodos[posV1].value)
 		if vec!=None:
 			for v in vec:
 				if v==v2:
-					grafo.vistos.clear()
+					graf.vistos.clear()
 					return True
 			for v in vec:
-				resp=existPath(grafo,v,v2)
+				resp=existPath(graf,v,v2)
 				if resp:
 					return resp
 		return False
-def isConnected(grafo:grafo):
-	nodos=grafo.nodos
+def isConnected(graf:grafo):
+	nodos=graf.nodos
 	for i in range(len(nodos)):
 		for j in range(len(nodos)):
 			if i!=j:
-				if not existPath(grafo,grafo.nodos[i].value,grafo.nodos[j].value):
-					grafo.vistos.clear()
+				if not existPath(graf,graf.nodos[i].value,graf.nodos[j].value):
+					graf.vistos.clear()
 					return False
 	return True
 
-def detectCycles(grafo:grafo,v,va):
-	pos=grafo._getPos(v)
+def _cycles(graf:grafo,v,va,ciclos:set):
+	pos=graf._getPos(v)
 	if pos!=None:
-		if not (grafo.nodos[pos].value in grafo.vistos):
-			vec=grafo.nodos[pos].vecinos
-			grafo.vistos.append(grafo.nodos[pos].value)
+		if not (graf.nodos[pos].value in graf.vistos):
+			vec=graf.nodos[pos].vecinos
+			graf.vistos.append(graf.nodos[pos].value)
+			graf.noVistos.remove(graf.nodos[pos].value)
 			if vec!=None:
 				for vn in vec:
 					if vn!=va:
-						resp=detectCycles(grafo,vn,v)
-						if resp!=None:
-							grafo.vistos.clear()
-							return resp
+						resp=_cycles(graf,vn,v,ciclos)
+						if resp:
+							# ciclos.append((v,vn))
+							ciclos.add(tuple(sorted((vn,v))))
 				return False
-			else:
-				return None
 		else:
 			return True
-def isTree(grafo:grafo):
-	return not detectCycles(grafo,grafo.nodos[randint(1,len(grafo.nodos))].value,None) and isConnected(grafo)
 
-def isComplete(grafo:grafo):
-	nNodes=len(grafo.nodos)
+def detectCycles(graf:grafo):
+	nodes=graf.noVistos.copy()
+	loop=set()
+	while graf.getNumNodos()>len(graf.vistos):
+		_cycles(graf,graf.nodos[graf.noVistos[randint(0,len(graf.noVistos)-1)]].value,None,loop)
+
+	graf.vistos.clear()
+	graf.noVistos=nodes.copy()
+	return loop
+	
+def isTree(graf:grafo):
+	return len(detectCycles(graf))>0 and isConnected(graf) and (graf.getNumAristas()==graf.getNumNodos()-1)
+
+def isComplete(graf:grafo):
+	nNodes=len(graf.nodos)
 	complete=False
-	for nodo in grafo.nodos:
+	for nodo in graf.nodos:
 		vecinos=nodo.vecinos
 		if len(vecinos)==(nNodes-1) and not (nodo.value in vecinos):
 			complete= True
 		else:
 			complete=False
 	return complete
-		
-grafo=createGraph([1,2,3,4,5,6,7],[(1,2),(1,3),(2,3),(2,4),(5,4),(6,4),(6,7)])
-print(existPath(grafo,5,7))
-print(grafo)
+def convertTree(graf:grafo):
+	return detectCycles(graf)
+grafo1=createGraph([1,2,3,4,5,6,7],[(1,2),(1,3),(2,3),(2,4),(5,4),(6,4),(6,7),(7,5)])
+print(existPath(grafo1,5,7))
+print(grafo1)
 
-print("Esta conectado?: ",isConnected(grafo))
-print("Hay ciclos? : ", detectCycles(grafo,4,None))
-print("Es Arbol? : ",isTree(grafo))
-print("Esta completo? : ",isComplete(grafo))
+print("Esta conectado?: ",isConnected(grafo1))
+print("Hay ciclos? : ", detectCycles(grafo1))
+print(grafo1.getNumAristas())
+print("Es Arbol? : ",isTree(grafo1))
+print("Esta completo? : ",isComplete(grafo1))
 
 grafoCompleto=createGraph([1,2,3,4],[(1,2),(1,3),(1,4),(2,3),(2,4),(3,4)])
 print(grafoCompleto)
+print(grafoCompleto.getNumAristas())
 print("Esta completo? : ",isComplete(grafoCompleto))
